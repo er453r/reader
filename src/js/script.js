0fa43aed144f1a9ff2c10199c0463bd69ade698d
Marcin Kotz <marcin.kotz@ives-system.com>
2020-08-30 01:38:50 +0200

    semi working

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,17 @@
+import * as Hypher from "hypher";
+import * as Polish from "hyphenation.pl";
+
 console.log('main script loaded')
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('dom loaded')
+
+  let h = new Hypher(Polish);
+
+  console.log(h.hyphenate('Dlaczego to nie działa a jednak jest poprawnie czekolada'))
+
+  console.log(h.hyphenate('jednak'))
+  console.log(h.hyphenate('jest'))
+  console.log(h.hyphenate('poprawnie'))
+  console.log(h.hyphenate('czekolada'))
 })
