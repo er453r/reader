dc824a19967a14205cea425c91b744fa0f06db9d
Marcin Kotz <marcin.kotz@ives-system.com>
2020-09-04 23:57:44 +0200

    sentence movement

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,3 @@
-import * as Hypher from "hypher";
-import * as Polish from "hyphenation.pl";
-
 console.log('main script loaded')
 
 const WORD = /^[\wżźćńółęąś]+/i
@@ -10,78 +7,152 @@ const WHITESPACE = /^[\s]+/
 
 const parts = [WORD, PAUSE, SENTENCE, WHITESPACE]
 
-function reader(input){
-  let sentenceStart = true
+function readerPrepare(input) {
+    let sentenceStart = true
 
-  const reader = document.querySelector("#reader")
+    const reader = document.querySelector("#reader")
 
-  iterator: while(input.length !== 0){
-    for (let i = 0; i < parts.length; i++){
-      const part = parts[i];
-      let match = input.match(part)
+    iterator: while (input.length !== 0) {
+        for (let i = 0; i < parts.length; i++) {
+            const part = parts[i];
+            let match = input.match(part)
 
-      if(match){
-        match = match[0]
+            if (match) {
+                match = match[0]
 
-        console.log(`Found match "${match}"`)
+                console.log(`Found match "${match}"`)
 
-        const p = document.createElement("span")
-        p.innerText = match
+                const p = document.createElement("span")
+                p.innerText = match
 
-        if(part === WORD){
-          console.log("Matched WORD")
+                if (part === WORD) {
+                    console.log("Matched WORD")
 
-          p.className = "reader word "
+                    p.className = "reader word "
 
-          if(sentenceStart){
-            p.className += "first-word"
+                    if (sentenceStart) {
+                        p.className += "first-word"
 
-            sentenceStart = false
-          }
+                        sentenceStart = false
+                    }
+                }
+
+                if (part === PAUSE) {
+                    console.log("Matched PAUSE")
+
+                    p.className = "reader pause"
+                }
+
+                if (part === SENTENCE) {
+                    console.log("Matched SENTENCE")
+
+                    p.className = "reader sentence"
+
+                    sentenceStart = true
+                }
+
+                if (part === WHITESPACE) {
+                    console.log("Matched WHITESPACE")
+
+                    p.className = "reader whitespace"
+                }
+
+                reader.append(p)
+
+                input = input.substr(match.length)
+
+                continue iterator
+            }
         }
 
-        if(part === PAUSE){
-          console.log("Matched PAUSE")
-
-          p.className = "reader pause"
-        }
-
-        if(part === SENTENCE){
-          console.log("Matched SENTENCE")
-
-          p.className = "reader sentence"
-
-          sentenceStart = true
-        }
-
-        if(part === WHITESPACE){
-          console.log("Matched WHITESPACE")
-
-          p.className = "reader whitespace"
-        }
-
-        reader.append(p)
-
-        input = input.substr(match.length)
-
-        continue iterator
-      }
+        throw new Error(`Could not find match for: "${input.substr(0, 20)}(...)"`)
     }
 
-    throw new Error(`Could not find match for: "${input.substr(0, 20)}(...)"`)
-  }
+    console.log("Parsed entire input!")
+}
 
-  console.log("Parsed entire input!")
+function readerNext() {
+    console.log("Reader next!")
+
+    const current = document.querySelector(".reader.current")
+
+    if (current === null) {
+        console.log("Trying to find first word...")
+
+        const startWord = document.querySelector(".reader.word")
+
+        console.log(`First word is "${startWord.innerHTML}"`)
+
+        startWord.className += " current"
+    } else {
+        console.log(`Found current: "${current.innerHTML}"`)
+
+        current.classList.remove("current")
+
+        let next = current.nextElementSibling
+
+        while (next != null) {
+            if (next.matches(".reader.word")) {
+                next.classList.add("current")
+
+                break
+            }
+            else
+                next = next.nextElementSibling
+        }
+
+        if(next)
+            console.log(`Next is: "${next.innerHTML}"`)
+    }
+}
+
+function readerPrevious() {
+    console.log("Reader previous!")
+
+    const current = document.querySelector(".reader.current")
+
+    if (current !== null) {
+        console.log(`Found current: "${current.innerHTML}"`)
+
+        current.classList.remove("current")
+
+        let previous = current.previousElementSibling
+
+        while (previous != null) {
+            if (previous.matches(".reader.word")) {
+                previous.classList.add("current")
+
+                break
+            }
+            else
+                previous = previous.previousElementSibling
+        }
+
+        if(previous)
+            console.log(`Previous is: "${previous.innerHTML}"`)
+    }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('DOM loaded')
+    console.log('DOM loaded')
 
-  // let h = new Hypher(Polish);
+    // let h = new Hypher(Polish);
 
-  // console.log(h.hyphenate('Dlaczego to nie działa a jednak jest poprawnie czekolada'))
+    // console.log(h.hyphenate('Dlaczego to nie działa a jednak jest poprawnie czekolada'))
 
-  const text = document.querySelector("#input").value
+    const text = document.querySelector("#input").value
 
-  reader(text)
+    readerPrepare(text)
+
+    readerNext()
 })
+
+document.onkeydown = event => {
+    console.log(`Pressed key: ${event.code}`)
+
+    if(event.code === "ArrowRight")
+        readerNext()
+
+    if(event.code === "ArrowLeft")
+        readerPrevious()
+}
